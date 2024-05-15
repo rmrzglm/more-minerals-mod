90bfe2dbf27024e329edbd7e24c913cc7e709c2c
Guillermo Ramirez <rmrz.glm@gmail.com>
2024-05-15 14:00:24 -0600

    Alpha ver. - 0.0.1a-1.19.4m

---
+++ src/main/java/net/vizragroup/mmm/More_Minerals_Mod.java
@@ -0,0 +1,15 @@
+package net.vizragroup.mmm;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class More_Minerals_Mod implements ModInitializer {
+	public static final String MODID = "mmm";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MODID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/vizragroup/mmm/More_Minerals_Mod_Client.java
@@ -0,0 +1,10 @@
+package net.vizragroup.mmm;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class More_Minerals_Mod_Client implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
